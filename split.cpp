88898f69a291550f8c1f22561976bcfd93eefc24
Theodora Heredia <theredia@usc.edu>
2025-01-30 18:49:26 +0000

    part 1 finished

--- split.cpp
+++ split.cpp
@@ -11,6 +11,7 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +19,29 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  // base case if node is empty
+  if(in == NULL){
+    return;
+  }
+  // copy head to a temp pointer so we can keep track of where the list starts
+  Node* temp = in;
+  // change the head pointer to point to the next node for recursion purposes
+  in = in->next;
+
+  // deference and access the value at the temp is pointing to 
+  if((temp->value % 2 == 0)){
+    // if it's even, then the temp needs to point to the evens head
+    temp->next = evens;
+    evens = temp;
+  }
+  else{
+    // if it's odd, then the temp needs to point to the odds head
+    temp->next = odds;
+    odds = temp;
+  }
+  // recursively call the split function for the next node
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,23 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  //Node* in = new Node(1, nullptr);
+  //in->next = new Node(2, nullptr);
+  //in->next->next = new Node(3, nullptr);
+
+  //Node* evens = NULL;
+  //Node* odds = NULL;
+
+  //split(in, evens, odds);
+
+  //cout << "evens: ";
+  
+
 
 }
